60ee9bbc63640d2142859428fbf3d676f06fd678
sercanatila <59374871+sercanatila@users.noreply.github.com>
2019-12-31 00:27:56 +0300

    Add files via upload

---
+++ proje_doktor.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: proje
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `doktor`
+--
+
+DROP TABLE IF EXISTS `doktor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doktor` (
+  `idDoktor` int(11) NOT NULL AUTO_INCREMENT,
+  `Doktorisim` varchar(20) DEFAULT NULL,
+  `Doktor_soyisim` varchar(20) DEFAULT NULL,
+  `poliklnik_id` int(11) DEFAULT NULL,
+  `Doktortc` varchar(11) DEFAULT NULL,
+  `Doktorsifre` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idDoktor`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doktor`
+--
+
+LOCK TABLES `doktor` WRITE;
+/*!40000 ALTER TABLE `doktor` DISABLE KEYS */;
+INSERT INTO `doktor` VALUES (1,'Ecem','Çiftçi',2,'10000000000','1'),(2,'Mahmut','Karayılan',1,'20000000000','2'),(3,'Yaprak','Çiçek',3,'30000000000','3'),(4,'Ali','Vefa',4,'40000000000','4'),(5,'Tarık','Akan',5,'50000000000','5'),(6,'Ömer Kadir','ERKEN',4,'60000000000','6'),(14,'Bünyamin','Yerleş',4,'55999444001','1234');
+/*!40000 ALTER TABLE `doktor` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-30 23:42:32
---
+++ proje_randevular.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: proje
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `randevular`
+--
+
+DROP TABLE IF EXISTS `randevular`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `randevular` (
+  `doktor_id` varchar(20) DEFAULT NULL,
+  `poliklinik_id` varchar(20) DEFAULT NULL,
+  `hasta_id` varchar(20) DEFAULT NULL,
+  `saat` varchar(20) DEFAULT NULL,
+  `tarih` varchar(20) DEFAULT NULL,
+  `randevuİd` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`randevuİd`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `randevular`
+--
+
+LOCK TABLES `randevular` WRITE;
+/*!40000 ALTER TABLE `randevular` DISABLE KEYS */;
+INSERT INTO `randevular` VALUES ('Mahmut Karayılan','Kulak Burun Boğaz','22334455667','11:00','2019-12-04',14),('Ecem Çiftçi','Göz','11223344556','10:00','2020-01-03',15);
+/*!40000 ALTER TABLE `randevular` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-30 23:42:32
